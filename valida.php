2bb3c94b6e74aeca416f55f012b6af2486564205
aripaiva <maverikjs@gmail.com>
2024-01-11 19:34:43 -0300

    Atualização teste 11/01/2024

--- valida.php
+++ valida.php
@@ -1,20 +1,22 @@
 <?php
 // Conectar ao banco de dados
-$servername = "23.111.140.162";
-$username = "basefort_junior";
-$password = "Jc.160574_@";
-$dbname = "basefort_banco";
+$servername = "localhost";
+$username = "basefort_ADM";
+$password = "Jjcr*)e}%~E2";
+$dbname = "basefort_BD";
 
-try{
-    $conn = new mysqli($servername, $username, $password, $dbname);
- }
- catch{
-    MessageBox.Show("Não estabeleceu conexão com o banco de dados");
- }
+$link = mysqli_connect("localhost", "basefort_ADM", "Jjcr*)e}%~E2", "basefort_BD");
+
+if (!$link) {
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
 // Verificar a conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
+if ($link->connect_error) {
+    die("Conexão falhou: " . $link->connect_error);
 }
 
 // Função para limpar e validar dados recebidos do formulário
@@ -26,28 +28,50 @@ function validar_dados($dados) {
 }
 
 // Receber dados do formulário
-$empresa = validar_dados($_POST['empresa']);
-$cnpj = validar_dados($_POST['cnpj']);
-$endereco = validar_dados($_POST['endereco']);
-$complemento = validar_dados($_POST['complemento']);
-$cidade = validar_dados($_POST['cidade']);
-$estado = validar_dados($_POST['estado']);
-$cep = validar_dados($_POST['cep']);
-$email = validar_dados($_POST['email']);
-$email = validar_dados($_POST['telefone']);
+$empresa = isset($_POST['empresa']) ? validar_dados($_POST['empresa']) : '';
+$cnpj = isset($_POST['cnpj']) ? validar_dados($_POST['cnpj']) : '';
+$endereco = isset($_POST['endereco']) ? validar_dados($_POST['endereco']) : '';
+$complemento = isset($_POST['complemento']) ? validar_dados($_POST['complemento']) : '';
+$cidade = isset($_POST['cidade']) ? validar_dados($_POST['cidade']) : '';
+$estado = isset($_POST['estado']) ? validar_dados($_POST['estado']) : '';
+$cep = isset($_POST['cep']) ? validar_dados($_POST['cep']) : '';
+$email = isset($_POST['email']) ? validar_dados($_POST['email']) : '';
+$telefone = isset($_POST['telefone']) ? validar_dados($_POST['telefone']): '';
 
 // Inserir dados no banco de dados usando prepared statement
-$stmt = $conn->prepare("INSERT INTO clientes (empresa, cnpj, endereco, complemento, cidade, estado, cep, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("ssssssss", $empresa, $cnpj, $endereco, $complemento, $cidade, $estado, $cep, $email);
+$stmt = $link->prepare("INSERT INTO clientes (empresa, cnpj, endereco, complemento, cidade, estado, cep, email, telefone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssssssss", $empresa, $cnpj, $endereco, $complemento, $cidade, $estado, $cep, $email, $telefone);
 
 // Executar a declaração preparada
 if ($stmt->execute()) {
     echo "Cadastro realizado com sucesso!";
+
+    // Enviar e-mail para a empresa
+    $destinatario = "vendas@baseforte.net.com";
+    $assunto = "Novo Cliente Cadastrado";
+    
+    $mensagem = "Novo cliente cadastrado:\n\n";
+    $mensagem .= "Empresa: $empresa\n";
+    $mensagem .= "CNPJ: $cnpj\n";
+    $mensagem .= "Endereço: $endereco\n";
+    $mensagem .= "Complemento: $complemento\n";
+    $mensagem .= "Cidade: $cidade\n";
+    $mensagem .= "Estado: $estado\n";
+    $mensagem .= "CEP: $cep\n";
+    $mensagem .= "E-mail: $email\n";
+    $mensagem .= "Telefone: $telefone\n";
+    
+    mail($destinatario, $assunto, $mensagem);
+
+     // Redirecionar para uma página de sucesso
+    header("Location: sucesso.html");
+    exit();
+    
 } else {
     echo "Erro: " . $stmt->error;
 }
 
 // Fechar a conexão
 $stmt->close();
-$conn->close();
+$link->close();
 ?>
